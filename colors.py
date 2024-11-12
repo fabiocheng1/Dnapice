96f7363bb2f889abd038372168ffddca36e09572
Rafael <88632576+fabiocheng1@users.noreply.github.com>
2024-11-12 17:18:22 -0300

    Add files via upload

---
+++ colors.py
@@ -0,0 +1,17 @@
+
+
+
+cores = {
+    "vermelho": "\033[31m" ,
+    "verde": "\033[32m" ,
+    "azul": "\033[34m" ,
+    "ciano": "\033[36m" ,
+    "magenta": '\033[35m' ,
+    "amarelo":"\033[33m" ,
+    "preto": '\033[30m' ,
+    "branco":'\033[37m' ,
+    "restaura":'\033[0;0m'
+}
+
+
+
